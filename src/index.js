e2fdf3f859ac3be5b569240a94d6c77a9d3eb028
roberto.torres <ts.roberto@gmail.com>
2020-10-16 23:11:56 +0100

    video and webgl canvas

--- src/index.js
+++ src/index.js
@@ -2,6 +2,36 @@ import vs from "./pos.vert";
 import shader from "./shader.frag";
 import * as THREE from "three";
 
+const btn = document.createElement("button");
+btn.innerHTML = "Start";
+btn.id = "btnStart";
+btn.style = "float: right;";
+document.body.appendChild(btn);
+
+const video = document.createElement("video");
+video.style = "float:left;";
+document.body.appendChild(video);
+
+const gl_div = document.createElement("div");
+gl_div.width = 620;
+gl_div.height = 480;
+
+document.body.appendChild(gl_div);
+
+btn.addEventListener("click", async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      facingMode: "user",
+      width: 640,
+      height: 480,
+    },
+  });
+
+  video.srcObject = stream;
+  await video.play();
+});
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
   75,
@@ -20,8 +50,8 @@ scene.add(plane);
 camera.position.z = 10;
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize(620, 480);
+gl_div.appendChild(renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
